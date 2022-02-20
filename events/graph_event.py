7efcee09da23485137596c52a6a731e239d70f78
bgiese <bengiese22@gmail.com>
2022-02-19 17:16:31 -0700

    enter symbol for graph

--- events/graph_event.py
+++ events/graph_event.py
@@ -6,9 +6,6 @@ from events.event import Event
 from stock_api import StockAPI
 from common import API_KEY
 import plotext as plt
-# from curses_config import CursesConfig
-# import termplotlib as tpl
-# import numpy as np
 
 class GraphEvent(Event):
 
@@ -17,7 +14,22 @@ class GraphEvent(Event):
         self.stock_api = StockAPI(API_KEY)
 
 
+    def specify_symbol(self) -> None:
+        valid_symbol = False
+        while not valid_symbol:   
+            self.window.erase()
+            self.window.refresh()
+            self.window.addstr(0,0, "Enter Stock Symbol\n--> ")
+            self.curses_config.echo()
+            self.curses_config.set_cursor(1)
+            _input = self.window.getstr().decode('utf-8-sig')
+            self.curses_config.set_cursor(0)
+            self.curses_config.noecho()
+            valid_symbol = self.stock_api.validate_symbol(_input)
+        globals.GRAPH_SYMBOL = _input.lower()
+
     def start_event(self):
+        self.specify_symbol()
         self.curses_config.disable()
         self.thread = Thread(target=self.run_event, name='graph_thread', args=(lambda: globals.KEY_LISTENER_HIT, ))
         self.thread.start()
@@ -34,20 +46,12 @@ class GraphEvent(Event):
 
     def run_event(self, key_listen):
         size = os.get_terminal_size()
-        # CursesConfig.pause_curses()
-        # self.window.erase()
-        # self.window.refresh()
         y = plt.sin() # sinusoidal signal 
         # plt.limit_size(size.columns-10, size.lines-10)
         plt.scatter(y)
-        plt.title("Scatter Plot")
+        plt.title(globals.GRAPH_SYMBOL.upper())
         plt.clear_terminal()
         plt.show()
-        # x = np.linspace(0, 2*np.pi, 100)
-        # y = np.sin(x) + x
-        # fig = tpl.figure()
-        # fig.plot(x,y, width=size.columns, height=size.lines)
-        # fig.show()
         while True:
             time.sleep(1.00 - ((time.time() - self.starttime) % 1.00))
             if key_listen():
